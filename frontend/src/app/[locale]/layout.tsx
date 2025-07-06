b20dd100cf34fb67434ae0839ead1c9b8a0a2ce1
Miguel Fernández Gargurevich <miguel@gargurevich.com>
2025-07-06 18:45:04 -0500

    fix(favicon): elimina referencia y archivo favicon.svg, solo PNG e ICO

--- frontend/src/app/[locale]/layout.tsx
+++ frontend/src/app/[locale]/layout.tsx
@@ -140,8 +140,13 @@ export default async function LocaleLayout({
   return (
     <html lang={locale} suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        {/* Favicons para máxima compatibilidad */}
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        {/* <link rel="icon" type="image/svg+xml" href="/favicon.svg" /> */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
